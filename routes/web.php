33349b8986c25401898c40ca9fc40463f936ffeb
Peeraphat Charoenthai <peeraphat.ch@kkumail.com>
2023-02-21 00:54:07 +0700

    add2

--- routes/web.php
+++ routes/web.php
@@ -47,16 +47,16 @@ Route::get('/trips/addCustomer/{id}', [TripController::class,'addCustomer']);
 
 
 
-Route::get('/test', [TestController::class, 'index'])->name('test');
-Route::get('/formTrip', [TestController::class, 'formTrip'])->name('formTrip');
-Route::get('/formCustomer', [TestController::class, 'formCustomer'])->name('formCustomer');
-Route::get('/billingPreview', [TestController::class, 'billingPreview'])->name('billingPreview');
-Route::get('/billingPrint', [TestController::class, 'billingPrint'])->name('billingPrint');
-Route::get('/invoicePreview', [TestController::class, 'invoicePreview'])->name('invoicePreview');
-Route::get('/invoicePrint', [TestController::class, 'invoicePrint'])->name('invoicePrint');
-Route::get('/quotationPreview', [TestController::class, 'quotationPreview'])->name('quotationPreview');
-Route::get('/quotationPrint', [TestController::class, 'quotationPrint'])->name('quotationPrint');
-Route::get('/calendar', [TestController::class, 'calendar'])->name('calendar');
+// Route::get('/test', [TestController::class, 'index'])->name('test');
+// Route::get('/formTrip', [TestController::class, 'formTrip'])->name('formTrip');
+// Route::get('/formCustomer', [TestController::class, 'formCustomer'])->name('formCustomer');
+// Route::get('/billingPreview', [TestController::class, 'billingPreview'])->name('billingPreview');
+// Route::get('/billingPrint', [TestController::class, 'billingPrint'])->name('billingPrint');
+// Route::get('/invoicePreview', [TestController::class, 'invoicePreview'])->name('invoicePreview');
+// Route::get('/invoicePrint', [TestController::class, 'invoicePrint'])->name('invoicePrint');
+// Route::get('/quotationPreview', [TestController::class, 'quotationPreview'])->name('quotationPreview');
+// Route::get('/quotationPrint', [TestController::class, 'quotationPrint'])->name('quotationPrint');
+// Route::get('/calendar', [TestController::class, 'calendar'])->name('calendar');
 
 // Route::get('/formTrip', [TestController::class, 'formTrip'])->name('formTrip');
 // Route::get('/formCustomer', [TestController::class, 'formCustomer'])->name('formCustomer');
